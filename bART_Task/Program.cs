afafc5234d7b8c13371ecdc10d754dd850cfbc64
Ihor <ihor.toporivskyi@gmail.com>
2022-08-30 21:06:35 +0300

    Initialized db with EF Core

--- bART_Task/Program.cs
+++ bART_Task/Program.cs
@@ -1,6 +1,14 @@
+using bART_Task.Domain;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<bARTDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("IntegratedDB"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
