b53ccd560629e90511503615406b2a8349583b38
Ihor <ihor.toporivskyi@gmail.com>
2022-08-30 20:07:58 +0300

    Initialized Domain models

---
+++ bART_Task.Domain/Models/Incident.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace bART_Task.Domain.Models
+{
+    public class Incident
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public virtual ICollection<Account> Accounts { get; set; }
+    }
+}
